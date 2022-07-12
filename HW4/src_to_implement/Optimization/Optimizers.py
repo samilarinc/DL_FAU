868c2faed3593ff4245d04b194700ae5970eb59f
samilarinc <msarinc@hotmail.com>
2022-07-12 15:40:42 +0200

    Optimizer fix

--- HW4/src_to_implement/Optimization/Optimizers.py
+++ HW4/src_to_implement/Optimization/Optimizers.py
@@ -13,7 +13,10 @@ class Sgd(Optimizer):
         self.learning_rate = learning_rate
     
     def calculate_update(self, weight_tensor, gradient_tensor):
-        temp = weight_tensor.copy()
+        if type(weight_tensor) is not np.ndarray:
+            temp = weight_tensor
+        else:
+            temp = weight_tensor.copy()
         weight_tensor -= self.learning_rate * gradient_tensor
         if self.regularizer is not None:
             weight_tensor -= self.learning_rate * self.regularizer.calculate_gradient(temp) 
@@ -27,7 +30,11 @@ class SgdWithMomentum(Optimizer):
         self.v = 0.
     
     def calculate_update(self, weight_tensor, gradient_tensor):
-        temp = weight_tensor.copy()
+        if type(weight_tensor) is not np.ndarray:
+            temp = weight_tensor
+        else:
+            temp = weight_tensor.copy()
+
         v = self.learning_rate * gradient_tensor + self.momentum_rate * self.v
         weight_tensor = weight_tensor - v
         self.v = v
@@ -46,7 +53,10 @@ class Adam(Optimizer):
         self.k = 1
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        temp = weight_tensor.copy()
+        if type(weight_tensor) is not np.ndarray:
+            temp = weight_tensor
+        else:
+            temp = weight_tensor.copy()
         self.v = self.mu * self.v + (1 - self.mu) * gradient_tensor
         self.r = self.rho * self.r + (1 - self.rho) * np.power(gradient_tensor, 2)
         v_hat = self.v / (1 - np.power(self.mu, self.k))
