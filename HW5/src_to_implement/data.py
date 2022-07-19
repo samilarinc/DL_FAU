422e50a41b7260f58cc6484cc9962962e0e4aa95
yarkin06 <yrknyldz@hotmail.com>
2022-07-19 19:30:23 +0200

    try

--- HW5/src_to_implement/data.py
+++ HW5/src_to_implement/data.py
@@ -14,26 +14,42 @@ class ChallengeDataset(Dataset):
     # TODO implement the Dataset class according to the description
     def __init__(self, data, mode):
         self.data = data
-        self.train = (mode == 'train')
+        # self.train = (mode == 'train')
+        self.mode = mode
         TF = tv.transforms
-        self._transform = TF.Compose([  
+        self.val_transform = TF.Compose([  
                                     TF.ToPILImage(),
                                     TF.ToTensor(),
                                     TF.Normalize(train_mean, train_std),
                                     # TF.RandomHorizontalFlip(p=0.3),
                                     # TF.RandomVerticalFlip(p=0.3)
                                     ])
+        self.train_transform = TF.Compose([  
+                                    TF.ToPILImage(),
+                                    TF.ToTensor(),
+                                    TF.Normalize(train_mean, train_std),
+                                    TF.RandomHorizontalFlip(p=0.3),
+                                    TF.RandomVerticalFlip(p=0.3)
+                                    ])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        data = self.data.iloc[idx]
-        img = imread(data['filename'], as_gray=True)
-        img = gray2rgb(img)
-        label = np.array([data['crack'], data['inactive']])
-        img = self._transform(img)
-        return img, label
+        if self.mode == "val":
+            data = self.data.iloc[idx]
+            img = imread(data['filename'], as_gray=True)
+            img = gray2rgb(img)
+            label = np.array([data['crack'], data['inactive']])
+            img = self.val_transform(img)
+            return img, label
+        if self.mode == "train":
+            data = self.data.iloc[idx]
+            img = imread(data['filename'], as_gray=True)
+            img = gray2rgb(img)
+            label = np.array([data['crack'], data['inactive']])
+            img = self.train_transform(img)
+            return img, label
 
     @property
     def transform(self):
