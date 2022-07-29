26052b0bf008424e255c0528a13db8d010a42b2c
yarkin06 <yrknyldz@hotmail.com>
2022-07-29 13:21:44 +0200

    try

---
+++ HW5/src_to_implement/try.py
@@ -0,0 +1,10 @@
+import torch as t
+from data import ChallengeDataset
+from trainer import Trainer
+from matplotlib import pyplot as plt
+import numpy as np
+import model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+trainer.restore_checkpoint(0, '../last/')
